2a890f5d1ab238985651f482caad7f5054e9843b
Jamshid Nuriddinov <jamshid346643@gmail.com>
2020-03-13 19:06:34 +0500

    new

---
+++ sql/ddl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+    id int ,
+    username VARCHAR(250),
+    email VARCHAR(250),
+    password VARCHAR(250),
+    fullname VARCHAR(250),
+    dob TIMESTAMP
+    PRIMARY KEY(id), 
+)
--- sql/imdb_queries.sql
+++ sql/imdb_queries.sql
@@ -24,20 +24,26 @@ JOIN movies_directors md ON d.id = md.director_id
 JOIN movies m ON m.id=md.movie_id
 WHERE m.name = 'Fight Club';
 
-SELECT COUNT(*) FROM movies m
+
+SELECT COUNT(*)
+FROM movies m
 JOIN movies_directors md ON md.movie_id=m.id
 JOIN directors d ON d.id=md.director_id
-WHERE d.first_name='Clint' AND d.last_name='Eastwood';
+WHERE d.first_name='Clint'
+    AND d.last_name='Eastwood';
 
 
-SELECT DISTINCT d.first_name FROM directors d 
+SELECT DISTINCT d.first_name
+FROM directors d
 JOIN directors_genres dj ON d.id=dj.director_id
 WHERE dj.genre = 'horror';
 
 
-SELECT a.first_name FROM actors a
+SELECT a.first_name
+FROM actors a
 JOIN roles r ON r.actor_id=a.id
 JOIN movies m ON m.id=r.movie_id
 JOIN movies_directors md ON md.movie_id=m.id
 JOIN directors d ON d.id = md.director_id
-WHERE d.first_name='Christopher' AND d.last_name='Nolan';
+WHERE d.first_name='Christopher'
+    AND d.last_name='Nolan';
